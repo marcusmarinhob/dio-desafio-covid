a55579e471e38e25a60164bffeb4e7757973a2c6
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2022-10-18 20:46:57 -0300

    Criando o primeiro gráfico dos dados selecionados

--- covid-analysis.py
+++ covid-analysis.py
@@ -1,24 +1,29 @@
-# Projeto COVID-19
-## Digital Innovation One
+#####################################################################################################
+#                                     Projeto COVID-19                                              #
+#                                  Digital Innovation One                                           #
+#####################################################################################################
 
-# Primeiro vamos importar algumas das bibliotecas necessárias para o projeto
+# Primeiro vamos importar algumas das bibliotecas necessárias para o projeto ########################
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objects as go 
 
-# Importando informações do CSV
+# print(pio.renderers.default)
+
+# Importando informações do CSV  ####################################################################
 file_path = './Suporte/covid_19_data.csv'
 df = pd.read_csv(file_path, parse_dates=['ObservationDate', 'Last Update'])
 print(df)
 
-# Conferir os tipos de cada coluna
+# Conferir os tipos de cada coluna ##################################################################
 print(df.dtypes)
 
-# Formatando as informações importadas
-#      - Nomes de colunas não devem ter letras maiúsculas e nem caracteres especiais. 
-#        Vamos implementar uma função para fazer a limpeza dos nomes dessas colunas.
+# Formatando as informações importadas ##############################################################
+#   Nomes de colunas não devem ter letras maiúsculas e nem caracteres especiais. 
+#   Vamos implementar uma função para fazer a limpeza dos nomes dessas colunas.
 
 import re
 
@@ -28,8 +33,25 @@ def corrige_colunas(col_name):
 # testando a função corrige_colunas(col_name)
 print( "\n" + corrige_colunas("Adge/P ou") + "\n") 
 
-# Vamos corrigir todas as colunas do df
+# Vamos corrigir todas as colunas do df 
 df.columns = [corrige_colunas(col) for col in df.columns]
 print(df.dtypes)
 
+# Pegando a lista dos países na base de dados ######################################################
+print( df.countryregion.unique() )
+
+# Vamos selecionar apenas os dados do Brasil para investigar #######################################
+print( df.loc[df.countryregion == 'Brazil'] )
+
+brasil = df.loc[
+    (df.countryregion == 'Brazil') &
+    (df.confirmed > 0)
+]
+print(brasil)
+
+# Casos Confirmados ################################################################################
+
+# Gráfico da evolução de casos confirmados
+fig = px.line(brasil, 'observationdate', 'confirmed', title='Casos confirmados no Brasil')
+fig.show()
 
