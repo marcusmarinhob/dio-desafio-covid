158b0ae77f8c31fe8babac026cadb633dbbd3265
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2022-10-18 21:06:49 -0300

    Calculando a taxa de crescimento média de casos confirmados no Brasil

--- covid-analysis.py
+++ covid-analysis.py
@@ -79,3 +79,34 @@ fig3.add_trace(
 fig3.update_layout(title='Mortes por COVID-19 no Brasil')
 fig3.show()
 
+# Taxa de crescimento ######################################################################################################
+# taxa_crescimento = (presente/passado)**(1/n)-1
+
+def taxa_crescimento(data, variable, data_inicio=None, data_fim=None):
+    # Se data inicio for None, define como a primeira data disponível
+    if data_inicio == None:
+        data_inicio = data.observationdate.loc[data[variable] > 0].min()
+    else:
+        data_inicio = pd.to_datetime(data_inicio)
+
+    if data_fim == None:
+        data_fim = data.observationdate.iloc[-1]
+    else:
+        data_fim = pd.to_datetime(data_fim)
+
+    # Define os valores do presente e passado
+    passado = data.loc[data.observationdate == data_inicio, variable].values[0]
+    presente = data.loc[data.observationdate == data_fim, variable].values[0]
+
+    # Define o número de pontso no tempo que vamos avaliar
+    n = (data_fim - data_inicio).days
+
+    # Calcular a taxa
+    taxa = (presente/passado)**(1/n) - 1
+
+    return taxa*100
+
+# Taxa de crescimento médio do COVID no Brasil em todo o período
+print(taxa_crescimento(brasil, 'confirmed'))
+
+
